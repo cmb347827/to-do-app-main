6a37f1ff0e2f2f5b40255ed79e453846cbfb4e1f
cmb347827 <v38d422ll621dem@gmail.com>
2025-08-26 17:13:23 -0400

    run clearlocalstorage once because of fixed key conflicts.

--- todo-app-main.js
+++ todo-app-main.js
@@ -446,7 +446,7 @@ const addlisteners=()=>{
 }
 
 $(window).on('load',function(){
-    //clearLocalStorage();
+    clearLocalStorage();
     let data = loadFromStorage(tasks);
 
     if(data ===null){
