74f40a666b4831b6b265eab77ec3822d9a1b1b9e
cmb347827 <v38d422ll621dem@gmail.com>
2025-08-27 21:44:52 -0400

    trying without changes

--- todo-app-main.js
+++ todo-app-main.js
@@ -362,12 +362,12 @@ const updateTaskContainer = (data) => {
                     which='';
                    }
                   (vars.tasksdiv.innerHTML += `
-                      <div class='drag-task' data-id='${taskId}'>
-                          <div class="display-flex align-items-center padding-half" id="${taskId}">
+                      <div data-id='${taskId}'>
+                          <div class="display-flex align-items-center" id="${taskId}">
                             <input onchange='setRemoveChecked(event)' class="checkbox-round me-1" type="checkbox" ${which} >
                             <label class='visually-hidden'>Check or uncheck task</label>
                             <textarea onchange='updateTask(event)' class="">${task}</textarea>
-                            <button onclick='deleteTask(event)' type='button' class='delete-task btn transparent-bg'><svg  class='cross' xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg></button>
+                            <button onclick='deleteTask(event)' type='button' class='delete-task transparent-bg'><svg  class='cross' xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg></button>
                           </div>
                           <hr class='bottom-hr'>
                       </div>
